1c000698c813c129d59077169d6deba79ec12a81
Graham Campbell <graham@mineuk.com>
2015-01-17 11:51:50 +0000

    Upgraded to the latest version of laravel 5

--- src/ExceptionsServiceProvider.php
+++ src/ExceptionsServiceProvider.php
@@ -11,7 +11,7 @@
 
 namespace GrahamCampbell\Exceptions;
 
-use Illuminate\Support\ServiceProvider;
+use Orchestra\Support\Providers\ServiceProvider;
 
 /**
  * This is the exceptions service provider class.
@@ -21,20 +21,13 @@ use Illuminate\Support\ServiceProvider;
 class ExceptionsServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
-     * Bootstrap the application events.
+     * Boot the service provider.
      *
      * @return void
      */
     public function boot()
     {
-        $this->package('graham-campbell/exceptions', 'graham-campbell/exceptions', __DIR__);
+        $this->addViewComponent('graham-campbell/exceptions', 'graham-campbell/exceptions', realpath(__DIR__.'/../views'));
     }
 
     /**
