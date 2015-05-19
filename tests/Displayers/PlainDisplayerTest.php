9aa5f4aaeaa708cf2395ea769ee14392bace9bbd
Graham Campbell <graham@mineuk.com>
2015-05-19 21:52:07 +0100

    Added a test for the plain displayer

---
+++ tests/Displayers/PlainDisplayerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Laravel Exceptions.
+ *
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\Tests\Exceptions\Displayers;
+
+use Exception;
+use GrahamCampbell\Exceptions\Displayers\PlainDisplayer;
+use GrahamCampbell\Tests\Exceptions\AbstractTestCase;
+use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
+
+/**
+ * This is the plain displayer test class.
+ *
+ * @author Graham Campbell <graham@mineuk.com>
+ */
+class PlainDisplayerProviderTest extends AbstractTestCase
+{
+    public function testExceptionHandlerIsInjectable()
+    {
+        $actual = (new PlainDisplayer())->display(new Exception('Oh noes!'), 502);
+
+        $expected = file_get_contents(__DIR__.'/stubs/plain.txt');
+
+        $this->assertSame($expected, $actual);
+    }
+}
