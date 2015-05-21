b102bfee278c1d1940d4a267563ea0204c1dc113
Graham Campbell <graham@mineuk.com>
2015-05-21 14:14:49 +0100

    Added tests for the can display filter

---
+++ tests/Filters/CanDisplayFilterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of Laravel Exceptions.
+ *
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\Tests\Exceptions\Filters;
+
+use Exception;
+use GrahamCampbell\Exceptions\Filters\CanDisplayFilter;
+use GrahamCampbell\Tests\Exceptions\AbstractTestCase;
+use Mockery;
+
+/**
+ * This is the can display filter test class.
+ *
+ * @author Graham Campbell <graham@mineuk.com>
+ */
+class CanDisplayFilterTest extends AbstractTestCase
+{
+    public function testFirstIsRemoved()
+    {
+        $exception = new Exception();
+        $html = Mockery::mock('GrahamCampbell\Exceptions\Displayers\HtmlDisplayer');
+        $html->shouldReceive('canDisplay')->once()->with($exception)->andReturn(false);
+        $json = Mockery::mock('GrahamCampbell\Exceptions\Displayers\JsonDisplayer');
+        $json->shouldReceive('canDisplay')->once()->with($exception)->andReturn(true);
+
+        $displayers = (new CanDisplayFilter())->filter([$html, $json], $exception);
+
+        $this->assertSame([$json], $displayers);
+    }
+
+    public function testNoChange()
+    {
+        $exception = new Exception();
+        $html = Mockery::mock('GrahamCampbell\Exceptions\Displayers\HtmlDisplayer');
+        $html->shouldReceive('canDisplay')->once()->with($exception)->andReturn(true);
+        $json = Mockery::mock('GrahamCampbell\Exceptions\Displayers\JsonDisplayer');
+        $json->shouldReceive('canDisplay')->once()->with($exception)->andReturn(true);
+
+        $displayers = (new CanDisplayFilter())->filter([$html, $json], $exception);
+
+        $this->assertSame([$html, $json], $displayers);
+    }
+}
