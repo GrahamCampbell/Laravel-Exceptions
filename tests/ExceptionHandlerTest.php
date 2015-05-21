53e9b68ddcc31179b01725b082e47c4b908a9b68
Graham Campbell <graham@mineuk.com>
2015-05-21 17:29:28 +0100

    Added tests for the exception handler

---
+++ tests/ExceptionHandlerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of Laravel Exceptions.
+ *
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\Tests\Exceptions;
+
+use Exception;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\GoneHttpException;
+
+/**
+ * This is the exception handler test class.
+ *
+ * @author Graham Campbell <graham@mineuk.com>
+ */
+class ExceptionHandlerTest extends AbstractTestCase
+{
+    public function testBasicRender()
+    {
+        $handler = $this->app->make('GrahamCampbell\Exceptions\ExceptionHandler');
+        $response = $handler->render($this->app->request, new Exception('Foo Bar.'));
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertSame(500, $response->getStatusCode());
+        $this->assertTrue(str_contains($response->getContent(), 'Foo Bar.'));
+        $this->assertSame('text/html', $response->headers->get('Content-Type'));
+    }
+
+    public function testNotFoundRender()
+    {
+        $handler = $this->app->make('GrahamCampbell\Exceptions\ExceptionHandler');
+        $response = $handler->render($this->app->request, new NotFoundHttpException());
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertSame(404, $response->getStatusCode());
+        $this->assertTrue(str_contains($response->getContent(), 'Not Found'));
+        $this->assertSame('text/html', $response->headers->get('Content-Type'));
+    }
+
+    public function testJsonRender()
+    {
+        $this->app->request->headers->set('accept', 'application/json');
+
+        $handler = $this->app->make('GrahamCampbell\Exceptions\ExceptionHandler');
+        $response = $handler->render($this->app->request, new GoneHttpException());
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertSame(410, $response->getStatusCode());
+        $this->assertSame('{"success":false,"code":410,"msg":"Houston, We Have A Problem"}', $response->getContent());
+        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+    }
+
+    public function testTextRender()
+    {
+        $this->app->request->headers->set('accept', 'not/acceptable');
+
+        $handler = $this->app->make('GrahamCampbell\Exceptions\ExceptionHandler');
+        $response = $handler->render($this->app->request, new NotFoundHttpException());
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $response);
+        $this->assertSame(500, $response->getStatusCode());
+        $this->assertSame('An error has occurred and this resource cannot be displayed.', $response->getContent());
+        $this->assertSame('text/plain', $response->headers->get('Content-Type'));
+    }
+}
