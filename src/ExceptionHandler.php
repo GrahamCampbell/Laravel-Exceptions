b2195f3094128de72805b01af066bdc69508dd55
Graham Campbell <graham@cachethq.io>
2015-05-23 15:02:41 +0100

    Updated my email

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Exceptions.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -20,7 +20,7 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * This is the exception hander class.
  *
- * @author Graham Campbell <graham@mineuk.com>
+ * @author Graham Campbell <graham@cachethq.io>
  */
 class ExceptionHandler extends Handler
 {
