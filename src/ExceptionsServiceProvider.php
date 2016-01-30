1d4427b3c14b7a0651157a18e8a5fa49ec9822e1
Graham Campbell <graham@alt-three.com>
2016-01-30 10:58:41 +0000

    Added missing import

--- src/ExceptionsServiceProvider.php
+++ src/ExceptionsServiceProvider.php
@@ -13,6 +13,7 @@ namespace GrahamCampbell\Exceptions;
 
 use GrahamCampbell\Exceptions\Displayers\HtmlDisplayer;
 use GrahamCampbell\Exceptions\Filters\VerboseFilter;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Lumen\Application as LumenApplication;
